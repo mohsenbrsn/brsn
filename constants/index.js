2f36bd6274016349e71ff44ec00295b131eacc13
mohsenbrsn <mohsen.boroosan@gmail.com>
2023-07-19 16:33:12 +0330

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x08333fa080556a3bd77ab2cb2947d7daf3466af8";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
